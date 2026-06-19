ce948fa6f5f4895872d2102c60be683278620a82
Sammy aniekan <sammy@Duke.devlad>
2026-06-19 20:06:27 +0100

    modified pwa and transaction status html

---
+++ assets/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "otex-v1";
+const urlsToCache = ["/", "/assets/css/user.css"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  // skip caching for websockets and API calls
+  if (
+    event.request.url.includes("/ws/") ||
+    event.request.url.includes("/api/")
+  ) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name)),
+      );
+    }),
+  );
+});
