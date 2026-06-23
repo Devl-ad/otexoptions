ae0971ced632736f4c04ab32a1a526c0ebc000ba
Sammy aniekan <sammy@Duke.devlad>
2026-06-23 08:28:14 +0100

    modified

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-    path('help-center/', views.help_center, name='help_center'),
-    path('careers/', views.careers, name='careers'),
-    path('faq/', views.faq, name='faq'),
-    path('roadmap/', views.roadmap, name='roadmap'),
-    path('legal-docs/', views.legal, name='legal'),
-    path('education/', views.education, name='education'),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("help-center/", views.help_center, name="help_center"),
+    path("careers/", views.careers, name="careers"),
+    path("faq/", views.faq, name="faq"),
+    path("roadmap/", views.roadmap, name="roadmap"),
+    path("legal-docs/", views.legal, name="legal"),
+    path("education/", views.education, name="education"),
+    path("privacy-policies/", views.privacy, name="privacy"),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -2,33 +2,40 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'index.html')
-
+    return render(request, "index.html")
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
 
 
 def contact(request):
-    return render(request, 'contacts.html')
+    return render(request, "contacts.html")
 
 
 def help_center(request):
-    return render(request, 'help_center.html')
+    return render(request, "help_center.html")
 
 
 def careers(request):
-    return render(request, 'careers.html')  
+    return render(request, "careers.html")
+
 
 def faq(request):
-    return render(request, 'faq.html')
+    return render(request, "faq.html")
+
 
 def roadmap(request):
-    return render(request, 'roadmap.html')
+    return render(request, "roadmap.html")
+
 
 def legal(request):
-    return render(request, 'legal-docs.html')
+    return render(request, "legal-docs.html")
+
 
 def education(request):
-    return render(request, 'education.html')
+    return render(request, "education.html")
+
+
+def privacy(request):
+    return render(request, "privacy.html")
